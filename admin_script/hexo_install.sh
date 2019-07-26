d3da884afeab17577284140e50353e5d60b47196
yifan <kelvinsail@163.com>
2019-07-26 18:09:07 +0800

    自动部署脚本

---
+++ admin_script/hexo_install.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+statu=$(cat is_install)
+if [ "$statu" = "false" ];then
+# 将已部署状态设置为true
+echo true > is_install
+
+# 重新生成静态文件
+cd /home/wwwroot/temp/hexo.source
+hexo clean && hexo g
+
+# 删除原有的站点文件夹中的文件
+rm -rf /home/wwwroot/blog.kelvinsail.com/*
+# 复制新的文件到站点目录中
+/bin/cp -rf public/* /home/wwwroot/blog.kelvinsail.com
+
+#mkdir /home/wwwroot/blog.old
+#chown jenkins:jenkins /home/wwwroot/blog.old
+#mkdir /home/wwwroot/blog.kelvinsail.com.new
+#/bin/cp -rf public/* /home/wwwroot/blog.kelvinsail.com.new
+#mv /home/wwwroot/blog.kelvinsail.com /home/wwwroot/blog.old/blog.kelvinsail.com."$(date '+%Y%m%d%H%M%S')"
+#mv /home/wwwroot/blog.kelvinsail.com.new /home/wwwroot/blog.kelvinsail.com
+
+fi
+
+
+
+
